6bcde58fda346bcbc336f9da6e6b4698e2a6879b
yanliang1 <1912988332@qq.com>
2019-03-21 22:14:11 +0800

    配置项目环境以及静态页面的整合

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * 存放项目中用到的配置信息，便于修改
+ */
+
+
+/**
+ * 数据库的主机
+ */
+ define('DB_HOST','localhost');
+/**
+* 数据库的用户名
+*/
+ define('DB_USER','root');
+/**
+* 数据库的密码
+*/
+ define('DB_PASS','123456');
+/**
+* 数据库的密码
+*/
+ define('DB_NAME','baixiu');
